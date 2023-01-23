f4cd1bc02e49e6f1b18a5a49e96aee55ffc1ddf2
Angela Amaral <angela.hawerroth@gmail.com>
2023-01-23 22:54:29 +0900

    Created a new file for the carousel javascript

--- javaScript/products.js
+++ javaScript/products.js
@@ -1,50 +1,10 @@
-// Style to images slider carousel
-const controls = document.querySelectorAll(".control");
-const images = document.getElementsByClassName("image");
-const maxImages = images.length;
-let currentImage = 0;
-
-controls.forEach((control) => {
-  control.addEventListener("click", () => {
-    // To know in which arrow the user clicked
-    const isLeft = control.classList.contains("arrow-left");
-
-    if (isLeft) {
-      currentImage -= 1;
-    }
-    else {
-      currentImage += 1;
-    }
-    // To not have negative index(currentImage)  or greater
-    if (currentImage >= maxImages) {
-      currentImage = 0;
-    }
-    if (currentImage < 0) {
-      currentImage = maxImages - 1;
-    }
-
-
-    Array.prototype.forEach.call(images, image => {
-      image.classList.remove("current-image");
-    });
-
-    images[currentImage].scrollIntoView({
-      behavior: "smooth",
-      inline: "center"
-    });
-
-    images[currentImage].classList.add("current-image")
-
-
-    console.log("control", isLeft, currentImage);
-  });
-});
-
-
 // Styles to products cards
-
 function addHighlight(helper) {
   helper.classList.add("highlighted");
+}
+
+function removeHighlight(helper) {
+  helper.classList.remove("highlighted");
 }
 
 // Event mouse card produto
@@ -58,6 +18,6 @@ for (let i = 0 ; i < cardProduct.length; i++) {
 }
 for (let i = 0 ; i < cardProduct.length; i++) {
   cardProduct[i].addEventListener("mouseout", (e) => {
-    cardProduct[i].remove("highlighted");
+    removeHighlight(cardProduct[i]);
   });
 }
