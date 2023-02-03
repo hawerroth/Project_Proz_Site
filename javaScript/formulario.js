e85a5db860fff4fb1b744a6e6443bee50ff60a43
rafaelnascimento369 <118895669+rafaelnascimento369@users.noreply.github.com>
2023-02-03 05:02:19 -0300

    Update formulario.js
    js

--- javaScript/formulario.js
+++ javaScript/formulario.js
@@ -1,2 +1,20 @@
 const form = document.getElementById('form')
 const campos = document.querySelectorAll('inputUser')
+ 
+function validarEmail(inputText)
+{
+var formatoEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+if(inputText.value.match(formatoEmail))
+{
+alert("Valid email address!");
+document.form1.text1.focus();
+return true;
+}
+else
+{
+alert("You have entered an invalid email address!");
+document.form1.text1.focus();
+return false;
+}
+
+}
